d238fda5aa375200abec9d55194d42dfd9e92ef9
Jorge Andres Escorcia <jaescorcia10@gmail.com>
2022-04-23 16:01:36 -0500

    Estructura del proyecto

---
+++ src/main/java/basepage/BasePage.java
@@ -0,0 +1,70 @@
+package pages.basepage;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class BasePage {
+
+    //Una sola instancia del WebDriver compartida para todas las clases
+    protected static WebDriver driver;
+    private static WebDriverWait wait;
+
+    static {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 10);
+
+
+    }
+
+    //Constructor para crear la instacia del driver una sola vez
+    public BasePage(WebDriver driver) {
+        BasePage.driver = driver;
+        PageFactory.initElements(driver,this);
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+
+    public static void navegarAFoodka(String url) {
+        driver.get(url);
+    }
+
+    private WebElement Find(String locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+    }
+
+    //Metodo click
+    public void clickElement(WebElement webElement){
+        webElement.click();
+    }
+
+    //Metodo Para Escrbir en los campos
+    public void escribirEnCampos(WebElement webElement, String texto){
+        webElement.clear(); //Limpiar el campo de texto
+        webElement.sendKeys(texto);
+    }
+
+    //Metodo cerra navegador
+    public void cerrarNavegador(){
+        driver.quit();
+
+    }
+
+    //Metodo para validarTexto
+    public String textFromElement(WebElement webElement){
+        return webElement.getText();
+    }
+}
+
+
+
