71754419e2a5b1e480a11ecafa50fd98e86871c2
Jorge Andres Escorcia <jaescorcia10@gmail.com>
2022-04-25 17:18:35 -0500

    Escenarios priorizados

---
+++ src/test/java/steps/ConsultarReservasSteps.java
@@ -0,0 +1,32 @@
+package steps;
+
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
+
+public class ConsultarReservasSteps {
+    @Dado("Que me encuentro en el modulo de consultar reserva")
+    public void que_me_encuentro_en_el_modulo_de_consultar_reserva() {
+
+    }
+
+    @Cuando("ingreso el codigo de la reserva en el campos correspondiente")
+    public void ingreso_el_codigo_de_la_reserva_en_el_campos_correspondiente() {
+
+    }
+
+    @Entonces("se consulte y se visualice la información de la reserva")
+    public void se_consulte_y_se_visualice_la_información_de_la_reserva() {
+
+    }
+
+    @Cuando("no ingreso el codigo de la reserva")
+    public void no_ingreso_el_codigo_de_la_reserva() {
+
+    }
+
+    @Entonces("debo observar un mensaje que me indique que no se pudo consultar la reserva")
+    public void debo_observar_un_mensaje_que_me_indique_que_no_se_pudo_consultar_la_reserva() {
+
+    }
+}
---
+++ src/test/resources/features/ConsultarReserva.feature
@@ -0,0 +1,15 @@
+# language: es
+Característica: Consultar Reservas
+  Yo como comensal
+  Requiero un botón donde pueda ver mis reservas
+  Para modificar la reserva o cancelarla
+  Antecedentes:
+    Dado Que me encuentro en el modulo de consultar reserva
+
+  Escenario:  Consultar reserva exitosa
+    Cuando ingreso el codigo de la reserva en el campos correspondiente
+    Entonces se consulte y se visualice la información de la reserva
+
+  Escenario:  Consulta de reserva fallida
+    Cuando no ingreso el codigo de la reserva
+    Entonces debo observar un mensaje que me indique que no se pudo consultar la reserva
